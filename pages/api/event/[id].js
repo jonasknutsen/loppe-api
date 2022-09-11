ef22bdbcb2e0352b76da41877d2e08d635efa9b8
Jonas Knutsen <jonas@j10n.no>
2022-09-11 18:24:46 +0200

    Adjusted query

--- pages/api/event/[id].js
+++ pages/api/event/[id].js
@@ -15,7 +15,7 @@ export default async function handler (req, res) {
     }
     switch (method) {
       case 'GET': {
-        db.oneOrNone('SELECT e.openingtimes, e.closingtimes, e.facebook, o.name as organizator_name, o.slug as organizator_slug, p.name as place_name, p.address, p.postcode, p.city, p.areas, p.coordinates, p.slug as place_slug FROM events e INNER JOIN organizers o ON e.organizer = o.id INNER JOIN places p ON e.place = p.id WHERE e.id = $1', [id])
+        db.oneOrNone('SELECT e.openingtimes, e.closingtimes, e.facebook, p.name as place, p.slug as place_slug, p.address, p.postcode, p.city, o.name as organizer, o.slug as organizer_slug, p.areas as event_areas FROM events e JOIN places p ON e.place = p.id JOIN organizers o ON e.organizer = o.id WHERE e.id = $1', [id])
           .then(event => {
             // success
             res.status(200).json(event)
