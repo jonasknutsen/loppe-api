04735481034f3c801e6a07341331f8ee106efb4e
Jonas Knutsen <jonas@j10n.no>
2023-04-14 20:13:36 +0200

    error

--- pages/api/events/index.js
+++ pages/api/events/index.js
@@ -18,29 +18,33 @@ export default async function handler (req, res) {
       return res.status(200).json({ events })
     }
     case 'POST': {
-      const openingTimestamps = openingTimes.map(ot => {
-        const day = ot.substring(0,2)
-        const month = ot.substring(2,4)
-        const year = '20' + ot.substring(4,6)
-        const hour = ot.substring(6,8)
-        const minutes = ot.substring(8,10)
-        // return new Date(+year, +month - 1, day, hour, minutes).toISOString()
-        return year + '-' + month + '-' + day + ' ' + hour + ':' + minutes + ':00+02'
-      })
-      const openingInsert = '{"' + openingTimestamps.toString().replace(',', '","') + '"}'
-      const closingTimestamps = closingTimes.map(ot => {
-        const day = ot.substring(0,2)
-        const month = ot.substring(2,4)
-        const year = '20' + ot.substring(4,6)
-        const hour = ot.substring(6,8)
-        const minutes = ot.substring(8,10)
-        // return new Date(+year, +month - 1, day, hour, minutes).toISOString()
-        return year + '-' + month + '-' + day + ' ' + hour + ':' + minutes + ':00+02'
-      })
-      const closingInsert = '{"' + closingTimestamps.toString().replace(',', '","') + '"}'
-      const rows = await db.query('INSERT INTO events(type, organizer, place, openingtimes, closingtimes, facebook) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [type, organizer, place, openingInsert, closingInsert, facebookEvent])
-      return res.status(200).json({ events: rows })
-      // return res.status(200).json({ great: 'success' })
+      try {
+        const openingTimestamps = openingTimes.map(ot => {
+          const day = ot.substring(0,2)
+          const month = ot.substring(2,4)
+          const year = '20' + ot.substring(4,6)
+          const hour = ot.substring(6,8)
+          const minutes = ot.substring(8,10)
+          // return new Date(+year, +month - 1, day, hour, minutes).toISOString()
+          return year + '-' + month + '-' + day + ' ' + hour + ':' + minutes + ':00+02'
+        })
+        const openingInsert = '{"' + openingTimestamps.toString().replace(',', '","') + '"}'
+        const closingTimestamps = closingTimes.map(ot => {
+          const day = ot.substring(0,2)
+          const month = ot.substring(2,4)
+          const year = '20' + ot.substring(4,6)
+          const hour = ot.substring(6,8)
+          const minutes = ot.substring(8,10)
+          // return new Date(+year, +month - 1, day, hour, minutes).toISOString()
+          return year + '-' + month + '-' + day + ' ' + hour + ':' + minutes + ':00+02'
+        })
+        const closingInsert = '{"' + closingTimestamps.toString().replace(',', '","') + '"}'
+        const rows = await db.query('INSERT INTO events(type, organizer, place, openingtimes, closingtimes, facebook) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [type, organizer, place, openingInsert, closingInsert, facebookEvent])
+        return res.status(200).json({ events: rows })
+      } catch (error) {
+        console.error(error)
+        return res.status(500).json({ error })
+      }
     }
     default: {
       res.setHeader('Allow', 'GET, POST')
